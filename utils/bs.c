f9e7bef4dabf5a986a6df316d6bbc50c1b7d6ec1
ryuuzaki <258483052@qq.com>
2013-07-29 21:04:11 +0800

    增加二分查找的C标准实现

---
+++ utils/bs.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+
+int *bsearch(int data[], int n, int x)
+{
+    int *p_rslt = NULL;
+    int *p_start = data;
+    int *p_mid = NULL;
+    int *p_end = data + n;
+
+    if ((NULL == data) || (n < 1)) {
+        goto EXIT;
+    }
+
+    while (p_start < p_end) {
+        p_mid = p_start + (p_end - p_start) / 2;
+
+        if (*p_mid > x) {
+            p_end = p_mid;
+        } else if (*p_mid < x) {
+            p_start = p_mid + 1;
+        } else {
+            p_rslt = p_mid;
+            break;
+        }
+    }
+
+EXIT:
+    return p_rslt;
+}
+
+int main(void)
+{
+    int i = 0;
+    int *px = NULL;
+    int ax[] = {23, 56, 88, 109, 333, 456, 551, 767, 845};
+
+    for (i = 0; i < (int)(sizeof(ax) / sizeof(ax[0])); ++i) {
+        px = bsearch(ax, sizeof(ax)/sizeof(ax[0]), ax[i]);
+        if (NULL == px) {
+            continue;
+        }
+        fprintf(stderr, "%d\n", *px);
+    }
+
+    return 0;
+}
--- utils/bs.cpp
+++ utils/bs.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <iostream>
-using namespace std;                            
+using namespace std;
 
 
 int binary_search(int array[],int n,int value)
@@ -11,24 +11,24 @@ int binary_search(int array[],int n,int value)
     int start = 0; // [start, stop)
     int stop = n;
     int mid = 0;
-    
+
     if (NULL == array) {
         return -1;
     }
     if (n < 1) {
         return -1;
     }
-    
+
     do {
         mid = start + (stop - start) / 2;
-        
+
         if (value == array[mid]) {
             rslt = value;
-            
+
             break;
         } else {
             rslt = -1;
-            if (mid == start) {            
+            if (mid == start) {
                 break;
             }
             if (value < array[mid]) {
@@ -38,25 +38,24 @@ int binary_search(int array[],int n,int value)
             }
         }
     } while (1);
-    
+
     return rslt;
 }
 
 
 //start 提示：自动阅卷起始唯一标识，请勿删除或增加。
 int main()
-{    
+{
     //write your code here
-    int xxx[] = {
-      1, 5, 8, 23, 44, 97, 126,  
-    };
-    
-    fprintf(stderr, "%d\n", binary_search(NULL, 0, 8));
-    fprintf(stderr, "%d\n", binary_search(xxx, 0, 8));
-    fprintf(stderr, "%d\n", binary_search(xxx, 1, 8));
-    fprintf(stderr, "%d\n", binary_search(xxx, sizeof(xxx) / sizeof(xxx[0]), 1));
-    fprintf(stderr, "%d\n", binary_search(xxx, sizeof(xxx) / sizeof(xxx[0]), 8));
-    fprintf(stderr, "%d\n", binary_search(xxx, sizeof(xxx) / sizeof(xxx[0]), 126));
-    fprintf(stderr, "%d\n", binary_search(xxx, sizeof(xxx) / sizeof(xxx[0]), 13));
+    int rslt;
+    int xxx[] = {23, 56, 88, 109, 333, 456, 551, 767, 845};
+
+    for (int i = 0; i < (int)(sizeof(xxx) / sizeof(xxx[0])); ++i) {
+        rslt = binary_search(xxx, sizeof(xxx)/sizeof(xxx[0]), xxx[i]);
+        if (-1 == rslt) {
+            continue;
+        }
+        fprintf(stderr, "%d\n", rslt);
+    }
 }
 //end //提示：自动阅卷结束唯一标识，请勿删除或增加。
