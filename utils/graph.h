512d38f4655a5978687e7cdfd7d5147477885e91
e7_wd <jackzxty@126.com>
2013-12-08 22:54:31 -0500

    制定图结构基本接口

--- utils/graph.h
+++ utils/graph.h
@@ -22,8 +22,24 @@ void list_remove(intptr_t **node)
 }
 
 
+typedef struct s_vertex vertex_t;
+struct s_vertex {
+    vertex_t *__vertex__;
+    intptr_t __list_node__;
+};
+
 typedef struct {
-    intptr_t __vertex__;
-    intptr_t __next__;
-} vertex_t;
+    vertex_t *__vertexes__;
+    intptr_t __vertex_count__;
+    intptr_t __edge_count__;
+    intptr_t *__cache_list__;
+} graph_t;
+extern intptr_t init_graph(graph_t *graph);
+extern vertex_t *graph_add_vertex(graph_t *graph);
+extern intptr_t graph_del_vertex(graph_t *graph, vertex_t *vertex);
+extern intptr_t graph_add_edege(graph_t *graph, vertex_t *from, vertex_t *to);
+extern intptr_t graph_del_edege(graph_t *graph, vertex_t *from, vertex_t *to);
+extern intptr_t graph_vertex_count(graph_t *graph);
+extern intptr_t graph_edege_count(graph_t *graph);
+extern void exit_graph(graph_t *gpaph);
 #endif // __GRAPH_H__
