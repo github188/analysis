94d8bf0f547f3048183c9f62f29bf2ccfadbd35c
e7_wd <jackzxty@126.com>
2013-12-15 13:23:28 -0500

    改善链表实现

--- utils/list.c
+++ utils/list.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <assert.h>
 
 
-#define ARRAY_COUNT(array)  (sizeof(array) / sizeof(array[0]))
+#define ARRAY_COUNT(array)  ((intptr_t)(sizeof(array) / sizeof(array[0])))
 #define OFFSET_OF(s, m)     ((size_t)&(((s *)0)->m ))
 #define CONTAINER_OF(ptr, type, member)     \
             ({\
@@ -11,24 +12,69 @@
                 (type *)((uint8_t *)p_mptr - OFFSET_OF(type, member));\
              })
 
+// arraydlist
+typedef struct {
+    intptr_t __prev__;
+    intptr_t __next__;
+} arraydlist_t;
 
-void list_add(intptr_t *head, intptr_t *node)
+void arraydlist_add(arraydlist_t *prev,
+                    arraydlist_t *next,
+                    arraydlist_t *node,
+                    intptr_t node_index)
 {
-    *node = *head;
-    *head = (intptr_t)node;
+    node->__next__ = prev->__next__;
+    node->__prev__ = next->__prev__;
+    prev->__next__ = node_index;
+    next->__prev__ = node_index;
 }
 
-void list_remove(intptr_t *node)
+void arraydlist_del(arraydlist_t *prev,
+                    arraydlist_t *next,
+                    arraydlist_t *node)
 {
-    intptr_t *p_del = (intptr_t *)*node;
+    prev->__next__ = node->__next__;
+    next->__prev__ = node->__prev__;
+    node->__next__ = 0;
+    node->__prev__ = 0;
+}
 
-    *node = *(intptr_t *)*node;
-    *p_del = 0;
+// arraylist
+void arraylist_add(intptr_t *prev, intptr_t *node, intptr_t node_index)
+{
+    *node = *prev;
+    *prev = node_index;
+}
+
+void arraylist_del(intptr_t *prev, intptr_t *node)
+{
+    *prev = *node;
+    *node = 0;
+}
+
+// list
+void list_add(intptr_t *prev, intptr_t *node)
+{
+    *node = *prev;
+    *prev = (intptr_t)node;
+
+    return;
+}
+
+void list_remove(intptr_t *prev)
+{
+    intptr_t *del = (intptr_t *)*prev;
+
+    *prev = *del;
+    *del = 0;
+
+    return;
 }
 
 typedef struct {
     int __x__;
     intptr_t __node__;
+    arraydlist_t __dnode__;
 } data_t;
 
 int list_rm(intptr_t *head, int d)
@@ -59,7 +105,7 @@ int main(int argc, char *argv[])
     int i = 0;
     intptr_t iter = 0;
 
-    for (i = 0; i < (intptr_t)ARRAY_COUNT(data); ++i) {
+    for (i = 0; i < ARRAY_COUNT(data); ++i) {
         data[i].__x__ = i;
         list_add(&list, &data[i].__node__);
     }
@@ -74,7 +120,11 @@ int main(int argc, char *argv[])
         (void)fprintf(stderr, "%d\n", data->__x__);
         iter = *(intptr_t *)iter;
     }
+
+    return 0;
 #endif
+
+#if 0
     intptr_t head = 0;
     intptr_t node = 0;
 
@@ -85,5 +135,48 @@ int main(int argc, char *argv[])
     (void)fprintf(stderr, "head: %x, node: %x\n", head, node);
 
     return 0;
-}
+#endif
 
+#if 1
+    intptr_t i = 0;
+    data_t data[8];
+
+    memset(data, 0, sizeof(data));
+    arraylist_add(&data[0].__node__, &data[2].__node__, 2);
+    arraylist_add(&data[2].__node__, &data[3].__node__, 3);
+    arraylist_add(&data[3].__node__, &data[7].__node__, 7);
+    arraylist_del(&data[0].__node__, &data[2].__node__);
+    for (i = 0; i < (intptr_t)ARRAY_COUNT(data); ++i) {
+        (void)fprintf(stderr, "data[%d] : %d\n", i, data[i].__node__);
+    }
+
+    memset(data, 0, sizeof(data));
+    arraydlist_add(&data[0].__dnode__,
+                   &data[0].__dnode__,
+                   &data[2].__dnode__,
+                   2);
+    arraydlist_add(&data[0].__dnode__,
+                   &data[2].__dnode__,
+                   &data[3].__dnode__,
+                   3);
+    arraydlist_add(&data[3].__dnode__,
+                   &data[2].__dnode__,
+                   &data[7].__dnode__,
+                   7);
+
+    /* that's NOT right, broke the dlist
+    arraydlist_add(&data[2].__dnode__,
+                   &data[3].__dnode__,
+                   &data[7].__dnode__,
+                   7);*/
+    for (i = 0; i < (intptr_t)ARRAY_COUNT(data); ++i) {
+        (void)fprintf(stderr,
+                      "data[%d] : (%d, %d)\n",
+                      i,
+                      data[i].__dnode__.__prev__,
+                      data[i].__dnode__.__next__);
+    }
+
+    return 0;
+#endif
+}
