a500770103e39db945e8ed139d45d141787d7584
lijia <258483052@qq.com>
2014-01-23 04:27:38 -0500

    增加删除边及结点功能

--- utils/graph.py
+++ utils/graph.py
@@ -3,6 +3,7 @@
 
 
 import sys
+import copy
 
 
 class graph:
@@ -22,6 +23,9 @@ class graph:
         def add_edge(self, to, weight):
             self.__edges[to] =  graph.vertex.edge(weight)
 
+        def exist_edge(self, to):
+            return to in self.__edges.keys()
+
         def del_edge(self, to):
             del self.__edges[to]
 
@@ -33,22 +37,52 @@ class graph:
     ''' graph '''
     def __init__(self):
         self.__vertexes = {}
+        self.__vertexes_indegree = {}
+
+    def __add_vertex(self, name):
+        self.__vertexes[name] = graph.vertex()
+        self.__vertexes_indegree[name] = 0
+
 
     def add_vertex(self, name):
-        self.__vertexes[name] = graph.vertex()
+        if name not in self.__vertexes.keys():
+            self.__add_vertex(name)
+
+    def del_vertex(self, name):
+        pass
 
     def add_edge(self, frm, to, weight = 0):
         if frm not in self.__vertexes.keys():
-            self.add_vertex(frm)
+            self.__add_vertex(frm)
+        if to not in self.__vertexes.keys():
+            self.__add_vertex(to)
+
+        if not self.__vertexes[frm].exist_edge(to):
+            self.__vertexes[frm].add_edge(to, weight)
+            self.__vertexes_indegree[to] += 1
+
+    def del_edge(self, frm, to):
+        if frm not in self.__vertexes.keys():
+            return -1
 
         if to not in self.__vertexes.keys():
-            self.add_vertex(to)
+            return -1
 
-        self.__vertexes[frm].add_edge(to, weight)
+        if not self.__vertexes[frm].exist_edge(to):
+            return -1
+
+        self.__vertexes[frm].del_edge(to)
+        self.__vertexes_indegree[to] -= 1
+
+        return 0
+
+    def topsort(self):
+        g = copy.deepcopy(self)
+        g.printme()
 
     def printme(self):
         for (k, v) in self.__vertexes.items():
-            sys.stdout.write("%s: " % k)
+            sys.stdout.write("%s(%d): " % (k, self.__vertexes_indegree[k]))
             v.printme()
 
 
@@ -56,4 +90,13 @@ if "__main__" == __name__:
     g = graph()
     g.add_edge("home", "school")
     g.add_edge("home", "company")
-    g.printme()
+    g.del_edge("home", "company")
+    g.add_edge("home", "company")
+    g.add_edge("home", "company")
+    g.add_edge("home", "company")
+
+    g.add_vertex("1")
+    g.add_vertex("2")
+    g.del_edge("1", "2")
+    g.add_edge("1", "2")
+    g.topsort()
