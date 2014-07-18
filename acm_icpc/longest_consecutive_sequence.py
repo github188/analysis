2dad43fde5cfec39874e6912c0ae1c4d9a782d5b
e7 <ryuuzaki.uchiha@gmail.com>
2014-07-18 06:56:01 +0000

    增加nginx1.0.15源码

---
+++ acm_icpc/longest_consecutive_sequence.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+class Solution:
+    def getMinMaxOfList(self, num):
+        min = 0
+        max = 0
+        pass
+
+    # @param num, a list of integer
+    # @return an integer
+    def longestConsecutive(self, num):
+        pass
+
+if "__main__" == __name__:
+    pass
