1e9f90636eb479da04e8bb7015c91c5ac76322a2
e7 <ryuuzaki.uchiha@gmail.com>
2014-07-23 08:27:04 +0000

    完成longest_consecutive_sequence算法

--- acm_icpc/longest_consecutive_sequence.py
+++ acm_icpc/longest_consecutive_sequence.py
@@ -3,37 +3,34 @@
 
 
 class Solution:
-    def getMinMaxOfList(self, num):
-        min = num[0]
-        max = num[0]
-
-        for x in num:
-            if x < min:
-                min = x
-            if x > max:
-                max = x
-
-        return (min, max)
-
     # @param num, a list of integer
     # @return an integer
     def longestConsecutive(self, num):
-        tmp_length = 0
         max_length = 0
+        dc = {}
 
-        (min, max) = self.getMinMaxOfList(num)
-        tmp_list = [0 for x in xrange(max - min + 1)]
         for x in num:
-            tmp_list[x - min] = 1
-        for n in tmp_list:
-            if 0 == n:
-                if tmp_length > max_length:
-                    max_length = tmp_length
-                tmp_length = 0
-            else:
+            dc[x] = 1
+        for key in dc:
+            keym1 = key - 1
+            keya1 = key + 1
+            tmp_length = 1
+
+            dc[key] = 0
+            while dc.has_key(keym1) and 1 == dc[keym1]:
+                dc[keym1] = 0
+                keym1 -= 1
+                tmp_length += 1
+            while dc.has_key(keya1) and 1 == dc[keya1]:
+                dc[keya1] = 0
+                keya1 += 1
                 tmp_length += 1
 
+            if tmp_length > max_length:
+                max_length = tmp_length
+
         return max_length
+
 
 if "__main__" == __name__:
     s = Solution()
