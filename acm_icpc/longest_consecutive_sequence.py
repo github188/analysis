c71b69878538b323f566f69703faad4f49efc079
e7 <ryuuzaki.uchiha@gmail.com>
2014-07-23 07:14:35 +0000

    完成longest_consecutive_sequence，但占用内存过大，还需改进

--- acm_icpc/longest_consecutive_sequence.py
+++ acm_icpc/longest_consecutive_sequence.py
@@ -4,14 +4,40 @@
 
 class Solution:
     def getMinMaxOfList(self, num):
-        min = 0
-        max = 0
-        pass
+        min = num[0]
+        max = num[0]
+
+        for x in num:
+            if x < min:
+                min = x
+            if x > max:
+                max = x
+
+        return (min, max)
 
     # @param num, a list of integer
     # @return an integer
     def longestConsecutive(self, num):
-        pass
+        tmp_length = 0
+        max_length = 0
+
+        (min, max) = self.getMinMaxOfList(num)
+        tmp_list = [0 for x in xrange(max - min + 1)]
+        for x in num:
+            tmp_list[x - min] = 1
+        for n in tmp_list:
+            if 0 == n:
+                if tmp_length > max_length:
+                    max_length = tmp_length
+                tmp_length = 0
+            else:
+                tmp_length += 1
+
+        return max_length
 
 if "__main__" == __name__:
-    pass
+    s = Solution()
+
+    pending = [10, 4, 20, 1, 3, 2]
+    print pending
+    print s.longestConsecutive(pending)
