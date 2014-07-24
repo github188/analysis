34b5a5378630ef3435c8d9e0eb424da34b815a8d
e7 <ryuuzaki.uchiha@gmail.com>
2014-07-24 09:32:31 +0000

    更新nginx_mem_pool.c

--- nginx/nginx_mem_pool.c
+++ nginx/nginx_mem_pool.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+typedef void (*ngx_pool_cleanup_pt)(void *data);
+typedef struct ngx_pool_large_s ngx_pool_large_t;
+typedef struct ngx_pool_data_s ngx_pool_data_t;
+typedef struct ngx_pool_s ngx_pool_t;
+
+struct ngx_pool_large_s {
+    ngx_pool_large_t *next;
+    void *alloc;
+};
+
+struct ngx_pool_data_s {
+    uint8_t *last;
+    uint8_t *end;
+    ngx_pool_t *next;
+    uintptr_t failed;
+};
 
 
 int main(int argc, char *argv[], char *env[])
